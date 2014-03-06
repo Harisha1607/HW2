12175ce6e44976a6586f7e1bd5c066429dd3d791
Nicholas Mancuso <nick.mancuso@gmail.com>
2014-03-06 15:56:40 -0500

    here are the updates you made during lecture.

--- CentralDogma/src/centraldogma/CentralDogma.java
+++ CentralDogma/src/centraldogma/CentralDogma.java
@@ -5,24 +5,29 @@ import org.biojava3.core.sequence.io.*;
 import org.biojava3.core.sequence.DNASequence;
 import org.biojava3.core.sequence.ProteinSequence;
 import org.biojava3.core.sequence.RNASequence;
+
 public class CentralDogma {
-     public static void main(String[] args) throws IOException, Exception{  
+    public static void main(String[] args) throws IOException, Exception{  
 
         if(args.length != 1){  
             System.out.println("Usage: DNA Sequences <filename>");  
             System.exit(1);  
-        }  
+        }   
 
         System.out.println("Opening file " + args[0] + " ...");
 
-        LinkedHashMap<String, DNASequence> a = FastaReaderHelper.readFastaDNASequence(new File(args[0]));
-        ProteinSequence protein = new DNASequence("ATG").getRNASequence().getProteinSequence();
-        writeProteinSequence(File translate.fa, Collection<ProteinSequence> proteinSequences);
-         
-             
-     }
+        LinkedHashMap<String, DNASequence> sequences = FastaReaderHelper.readFastaDNASequence(new File(args[0]));
+        ArrayList<ProteinSequence> proteins = new ArrayList<ProteinSequence>();
 
-    
-    }
-
-
+        // iterate over the linked hash map
+        for (DNASequence seq : sequences.values()) {
+            //     DNA->RNA->Protein
+            ProteinSequence protein = seq.getRNASequence().getProteinSequence();  
+            //     add new protein to the protein-list
+            proteins.add(protein);    
+        }   
+        // make a new file for output [hard-coded name is fine]
+        File translatedFile = new File("translate.fa");
+        FastaWriterHelper.writeProteinSequence(translatedFile, proteins);
+    }   
+}
